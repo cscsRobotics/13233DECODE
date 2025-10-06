1379dbc78ee597a946ceab99f6898eb549f1414e
David Mello <theycallmedavidmello@gmail.com>
2025-10-05 22:38:28 -0400

    Updated class name to be more descriptive and did the same for methods, added more comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -6,50 +6,32 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "TeleOpMain", group = "Drive")
 public class TeleOpMain extends LinearOpMode {
-
-
     /**
-     * This function is executed when this Op Mode is selected from the Driver Station.
+     * This function is executed when this Op Mode is selected from the Driver Station
      */
     @Override
     public void runOpMode() {
+        CommonControls CommonControls = new CommonControls(hardwareMap);
 
-        cyberknights cyberknights = new cyberknights(hardwareMap);
-
-
-        //****************************************//
-        // Map all robot hardware				  //
-        //****************************************//
-
-
-
-
-
-        //The waitForStart() function will wait for the start button will begin
-        //DON'T WRITE ANY CODE AFTER THE WAIT FOR START UNTIL THE "while (opModIsActive())"
-        //THIS WILL CAUSE PROBLEMS WHEN GOING THROUGH INSPECTION
+        // The waitForStart() function will wait for the start button to begin
+        // DON'T WRITE ANY CODE AFTER THE WAIT FOR START UNTIL THE "while (opModIsActive())"
+        // THIS WILL CAUSE PROBLEMS WHEN GOING THROUGH INSPECTION
         waitForStart();
 
-        //******************************************//
-        // Run code while op mode is active			//
-        //******************************************//
+        // Run code while op mode is active
         while (opModeIsActive()) {
-            //**************************************************************//
-            //Telemetry Code the stuff that appears on the right side of the//
-            //driver hub													//
-            //**************************************************************//
+            // Add status data to driver hub display
             telemetry.addData("Status", "opModeIsActive");
             telemetry.update();
 
-            //regular drive controls
-            cyberknights.allDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            // Sets the power to the drive motors based on current gamepad inputs
+            CommonControls.setDrivePower(gamepad1.left_stick_y, gamepad1.left_stick_x,
+                gamepad1.right_stick_x);
 
-            //Intake Control
-            cyberknights.intake(gamepad2.left_bumper, gamepad2.right_bumper);
+            // Ball ("artifact"/game element) intake control
+            CommonControls.setIntakeDirection(gamepad2.left_bumper, gamepad2.right_bumper);
 
         }
-        //NO DRIVE CODE OUT SIDE OF THE OPMODEACTIVE LOOP WILL CAUSE PROBLEMS IN INSPECTION
+        // Do not place any drive code here outside of the while loop (will fail inspection)
     }
-
-
 }
