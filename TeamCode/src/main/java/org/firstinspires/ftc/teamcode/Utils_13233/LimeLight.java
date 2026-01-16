4ebac40f00775f816a914a550676cc63893ee0da
gatorgrad96 <jacobmm11901@gmail.com>
2026-01-15 21:32:23 -0500

    limelight
    yes its still a buggy mess
    need to import limelight sdk but im getting closer to fixing issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils_13233/LimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils_13233/LimeLight.java
@@ -1,22 +1,62 @@
 package org.firstinspires.ftc.teamcode.Utils_13233;
 
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.IMU;
 
-public abstract class LimeLight extends LinearOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.limelightvision.limelight3a.*;
+
+@TeleOp(name = "Limelight 3A AprilTag Test", group = "Vision")
+public class LimelightAprilTagTest extends LinearOpMode {
+
     private Limelight3A limelight;
-    private IMU imu;
+
     @Override
-    public void init() {
-        limelight = hardwareMap.get(Limelight3A.class,"limelight");
-        limelight.pipelineSwitch(8); //remember to change limelight pipeline using browser
-        imu = hardwareMap.get(IMU.class,"imu");
-        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot.LogoFacingDirection(RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
-            RevHubOrientationOnRobot.UsbFacingDirection.UP);
-        imu.initalize(new IMU.Parameters(revHubOrientationOnRobot));
-    public void start() {
+    public void runOpMode() throws InterruptedException {
+
+        // ===== INIT LIMELIGHT =====
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        // Pipeline must be an AprilTag pipeline (set in Limelight UI)
+        limelight.pipelineSwitch(8);
+
         limelight.start();
+
+        telemetry.addLine("Limelight initialized");
+        telemetry.addLine("Waiting for start...");
+        telemetry.update();
+
+        waitForStart();
+
+        // ===== MAIN LOOP =====
+        while (opModeIsActive()) {
+
+            LimelightResults results = limelight.getLatestResults();
+
+            if (results != null && results.isValid() && results.targets.size() > 0) {
+
+                for (LimelightTarget target : results.targets) {
+
+                    if (target.getType() == LimelightTargetType.APRILTAG) {
+
+                        telemetry.addLine("=== APRILTAG DETECTED ===");
+                        telemetry.addData("Tag ID", target.getFiducialID());
+                        telemetry.addData("X Offset", target.getX());
+                        telemetry.addData("Y Offset", target.getY());
+                        telemetry.addData("Area", target.getArea());
+                        telemetry.addData("Skew", target.getSkew());
+                        telemetry.addData("Latency (ms)", results.latency);
+                        break;
+                    }
+                }
+
+            } else {
+                telemetry.addLine("No AprilTag detected");
+            }
+
+            telemetry.update();
+        }
+
+        limelight.stop();
     }
 }
